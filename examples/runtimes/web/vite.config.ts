01ac54434743a13d9a96860a5dff58c496b5c9cf
Aaron Colby <acolby@live.com>
2026-09-18 10:07:29 -0700

    Make runtime iframe explicit in web example

--- examples/runtimes/web/vite.config.ts
+++ examples/runtimes/web/vite.config.ts
@@ -3,4 +3,12 @@ import preact from "@preact/preset-vite";
 
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    rollupOptions: {
+      input: {
+        demo: "index.html",
+        runtime: "runtime.html",
+      },
+    },
+  },
 });
